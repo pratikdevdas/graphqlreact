586b7bca95629a819efbb30a62b6d526ac4e90fc
Revenant <dasdev.pratik@gmail.com>
2022-08-26 17:57:28 +0530

    initial-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import App from './App'
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const query = gql`
+query {
+  allPersons {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+client.query({ query })
+  .then((response) => {
+    console.log(response.data, 'cake')
+  })
+
+ReactDOM.render(<App />, document.getElementById('root'))
