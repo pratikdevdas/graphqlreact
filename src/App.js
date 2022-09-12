22f41e84c3df98658dc5cc521df8c2899ede2b58
Revenant <dasdev.pratik@gmail.com>
2022-09-12 16:20:00 +0530

    Apollo-client: Doing Mutation And Updating Cache

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { gql, useQuery } from '@apollo/client'
 import Persons from './Persons'
+import PersonForm from './PersonForm'
 
 const ALL_PERSONS = gql`
 query{
@@ -11,13 +12,16 @@ query{
 }`
 
 function App() {
-  const result = useQuery(ALL_PERSONS)
+  const result = useQuery(ALL_PERSONS, {
+    pollInterval: 2000
+  })
 
   if ( result.loading){
     return <div>loading...</div>
   }
   return (
     <div className="App">
+      <PersonForm />
   <Persons persons={result.data.allPersons}/>
     </div>
   );  
